e8f1546611259ad1a6266d56e2dd8aa0e804cfb8
melkiurak <melkiurak3209@gmail.com>
2026-01-10 17:31:36 +0200

    Add checbox

--- src/components/Filters/Filters.jsx
+++ src/components/Filters/Filters.jsx
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 export default function Filters ({date}) {
+    const [selectedYears, setSelectedYears] = useState([]);
     //const actives = ["Active", "Archived", "Draft"]
     const currentYear = new Date().getFullYear()
     const years = [];
+
     for (let i = 2000; i < currentYear; i++){
         years.push(i)
     }
-    console.log(years)
+    const handelChangeYear = (year, e) => {
+        const value = parseInt(e.target.value);
+        if(e.target.checked){
+            setSelectedYears(prev => [...prev, value]);
+        }
+        else {
+            setSelectedYears(selectedYears.filter(y => y !== value))        
+        }
+    }
     return (
         <div>
-            <div>
-                <input type="text" name="" id="" value={date} />
-            </div>
+            {years.map(year => (
+                <lable key={year} style={{display: 'block'}}>
+                    <input type="checkbox" value={year} checked={selectedYears.includes(year)} onChange={(e) => handelChangeYear(year, e)}/>
+                    <span>{year}</span>
+                </lable>
+            ))}
         </div>
     )
 }
